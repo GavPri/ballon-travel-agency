b8f326c6139fddeca85c218e3078dcb44bbe20cb
德华 刘 <gavinprior5@gmail.com>
2024-04-18 12:18:59 +0000

    comment in structure of nav

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-  return <nav className="max-w-[1240px] bg-dark h-24 m-auto"></nav>;
+  return <nav className="max-w-[1240px] bg-dark h-24 m-auto">
+   {/* ----- Logo */}
+   {/* ----- Nav links / unordered list */}
+   {/* ----- Social media links */}
+  </nav>;
 };
 
 export default Navbar;
