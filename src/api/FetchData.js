14c48046a7b9b70aa3d8ee7464d2dfbaac2e022d
德华 刘 <gavinprior5@gmail.com>
2024-04-18 20:59:49 +0000

    fetch data and return data, is loading and errors

--- src/api/FetchData.js
+++ src/api/FetchData.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const FetchData = (endpoint) => {
   //  Define state variables to manage fetched data, loading state, and errors
@@ -10,7 +11,31 @@ const FetchData = (endpoint) => {
     // Define an asynchronous function to fetch data from the specified endpoint
     const fetchData = async () => {
       try {
-      } catch (error) {}
+        // Make a GET request using Axios to fetch data from the specified endpoint
+        const response = await axios.get(
+          `https://jsonplaceholder.typicode.com/${endpoint}`,
+          //   Update the data state variable with the fetched data
+          setData(response.data)
+        );
+      } catch (errors) {
+        // If an error occurs during fetching, set the error state variable
+        setErros(errors);
+      } finally {
+        // Set the loading state variable to false once fetching is complete, regardless of success or failure
+        setIsLoading(false);
+      }
     };
-  });
+
+    // Call the fetchData function when the component mounts or when the endpoint changes
+    fetchData();
+
+    return () => {
+      // Clean up function if required
+    };
+  }, [endpoint]);
+
+  // Return the fetched data, loading state, and error state for use in the component
+  return { data, isLoading, errors };
 };
+
+export default FetchData;
