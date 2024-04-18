d4d9724a8df18832592b360368eadfa879841358
德华 刘 <gavinprior5@gmail.com>
2024-04-18 13:21:30 +0000

    adjust x axis padding on nav

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -5,7 +5,7 @@ import SocialMediaLinks from "./SocialMediaLinks";
 
 const Navbar = () => {
   return (
-    <nav className="w-screen bg-dark h-24 m-auto flex items-center justify-between absolute top-0 px-6">
+    <nav className="w-screen bg-dark h-24 m-auto flex items-center justify-between absolute top-0 px-10">
       {/* ----- Logo */}
       <Logo width={100} height={100} />
       {/* ----- Nav links / unordered list */}
