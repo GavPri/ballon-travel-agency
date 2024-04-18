80dd827667b2f385b82f54124822ed9c218dc951
德华 刘 <gavinprior5@gmail.com>
2024-04-18 11:57:03 +0000

    configure color theme in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      dark: "#07130d",
+      amber: "#ffef62",
+      yellow: "#ffd600",
+      white: "#f7f2e6",
+      grey: "#b1b1b1",
+    },
     extend: {
       fontFamily: {
         M: ["Merienda", "sans-serif"],
