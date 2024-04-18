e39e1731a5644367795531d0a2c45dd31047984d
德华 刘 <gavinprior5@gmail.com>
2024-04-18 12:28:35 +0000

    create logo component
    import logo into navbar

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Logo = ({ width, height, classname }) => {
+  return (
+    <img
+      width={width}
+      height={height}
+      src="/logoballon.png"
+      alt="Logo"
+    />
+  );
+};
+
+export default Logo;
--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Logo from "../Logo";
 
 const Navbar = () => {
-  return <nav className="max-w-[1240px] bg-dark h-24 m-auto">
-   {/* ----- Logo */}
-   {/* ----- Nav links / unordered list */}
-   {/* ----- Social media links */}
-  </nav>;
+  return (
+    <nav className="max-w-[1240px] bg-dark h-24 m-auto">
+      {/* ----- Logo */}
+      <Logo width={100} height={100} />
+      {/* ----- Nav links / unordered list */}
+      {/* ----- Social media links */}
+    </nav>
+  );
 };
 
 export default Navbar;
