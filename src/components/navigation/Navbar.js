b7b3b805757fc35304d0f3afb92123453a35bf59
德华 刘 <gavinprior5@gmail.com>
2024-04-18 12:53:58 +0000

    add follow us div to nav
    items center moved to nav from ul

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -3,11 +3,11 @@ import Logo from "../Logo";
 
 const Navbar = () => {
   return (
-    <nav className="max-w-[1240px] bg-dark h-24 m-auto flex">
+    <nav className="max-w-[1240px] bg-dark h-24 m-auto flex items-center justify-between">
       {/* ----- Logo */}
       <Logo width={100} height={100} />
       {/* ----- Nav links / unordered list */}
-      <ul className="text-grey flex items-center font-Fig">
+      <ul className="text-grey flex  font-Fig">
         <li className="mr-6 hover:cursor-pointer hover:text-yellow transition-all duration-500 ease-in-out">
           Home
         </li>
@@ -28,6 +28,12 @@ const Navbar = () => {
         </li>
       </ul>
       {/* ----- Social media links */}
+      <div>
+        <p className="font-Fig text-grey">Follow Us :</p>
+        <li></li>
+        <li></li>
+        <li></li>
+      </div>
     </nav>
   );
 };
