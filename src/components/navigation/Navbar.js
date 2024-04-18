491c7d4042167c298a18a17c72f0595c03781257
德华 刘 <gavinprior5@gmail.com>
2024-04-18 13:19:24 +0000

    add absolute positioning to nav bar, adjust width

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -5,7 +5,7 @@ import SocialMediaLinks from "./SocialMediaLinks";
 
 const Navbar = () => {
   return (
-    <nav className="max-w-[1240px] bg-dark h-24 m-auto flex items-center justify-between">
+    <nav className="w-screen bg-dark h-24 m-auto flex items-center justify-between absolute top-0 px-6">
       {/* ----- Logo */}
       <Logo width={100} height={100} />
       {/* ----- Nav links / unordered list */}
