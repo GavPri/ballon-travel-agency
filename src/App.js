85e5b6c53a5131a6b34579d0e7a46b89b7fa4e9a
德华 刘 <gavinprior5@gmail.com>
2024-04-18 18:17:39 +0000

    adjust navbar for ul

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import Hero from "../src/pages/homepage/Hero";
 
 function App() {
   return (
-    <div className="bg-dark min-h-screen">
-      <div className="max-w-[1240px] m-auto">
-        <Navbar />
-        <Hero />
-      </div>
+    <div className="max-w-[1240px] m-auto">
+      <Navbar />
+      <Hero />
     </div>
   );
 }
--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -12,53 +12,65 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex justify-center m-auto w-[80vw]">
-      <nav className="w-[80vw] md:max-w-[1240px] h-24 bg-transparent z-10 m-0 flex items-center justify-between absolute top-0 ">
-        {/* ----- Logo */}
-        <Logo width={100} height={100} />
-        {/* ----- Nav links / unordered list */}
-        <NavbarLinks />
-        {/* ----- Social media links */}
-        <SocialMediaLinks />
-        {/* ----- hamburger menu */}
-        <div
-          className="ml-auto w-6 aspect-square relative md:hidden"
-          onClick={handleMobileMenu}
-        >
-          {/* ---- first & last span - opacity 0, width 0 when open */}
-          <span
-            className={
-              mobileMenu
-                ? `opacity-0 w-0 bg-yellow h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-0/4`
-                : `bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-0/4`
-            }
-          ></span>
-          {/* ----- Rotate 45 deg when open */}
-          <span
-            className={
-              mobileMenu
-                ? `-rotate-45 bg-yellow w-full h-0.5 rounded-full absolute transition-all duration-500 ease-in-out top-1/4`
-                : `bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-1/4`
-            }
-          ></span>
-          {/* ----- Rotate -45 deg when open */}
-          <span
-            className={
-              mobileMenu
-                ? `rotate-45 bg-yellow w-full h-0.5 rounded-full absolute transition-all duration-500 ease-in-out top-1/4`
-                : `bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-1/4`
-            }
-          ></span>
-          {/* ---- first & last span - opacity 0, width 0 when open */}
-          <span
-            className={
-              mobileMenu
-                ? `opacity-0 w-0 bg-yellow h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-2/4`
-                : `bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-2/4`
-            }
-          ></span>
-        </div>
-      </nav>
+    <div className="w-screen md:max-w-[1240px]">
+      {/* ----Navigation Wrapper */}
+      <div className="flex justify-center items-center ">
+        <nav className="w-[80vw] md:max-w-[1240px] h-24 bg-transparent z-10 m-0 flex items-center justify-between absolute top-0 ">
+          {/* ----- Logo */}
+          <Logo width={100} height={100} />
+          {/* ----- Nav links / unordered list */}
+          <NavbarLinks />
+          {/* ----- Social media links */}
+          <SocialMediaLinks />
+          {/* ----- hamburger menu */}
+          <div
+            className="ml-auto w-6 aspect-square relative md:hidden"
+            onClick={handleMobileMenu}
+          >
+            {/* ---- first & last span - opacity 0, width 0 when open */}
+            <span
+              className={
+                mobileMenu
+                  ? `opacity-0 w-0 bg-yellow h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-0/4`
+                  : `bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-0/4`
+              }
+            ></span>
+            {/* ----- Rotate 45 deg when open */}
+            <span
+              className={
+                mobileMenu
+                  ? `-rotate-45 bg-yellow w-full h-0.5 rounded-full absolute transition-all duration-500 ease-in-out top-1/4`
+                  : `bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-1/4`
+              }
+            ></span>
+            {/* ----- Rotate -45 deg when open */}
+            <span
+              className={
+                mobileMenu
+                  ? `rotate-45 bg-yellow w-full h-0.5 rounded-full absolute transition-all duration-500 ease-in-out top-1/4`
+                  : `bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-1/4`
+              }
+            ></span>
+            {/* ---- first & last span - opacity 0, width 0 when open */}
+            <span
+              className={
+                mobileMenu
+                  ? `opacity-0 w-0 bg-yellow h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-2/4`
+                  : `bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-2/4`
+              }
+            ></span>
+          </div>
+          {/* ----- Side Bar NavList */}
+        </nav>
+      </div>
+      <ul className="absolute top-16 w-[100vw] h-[100vh] bg-dark text-white z-50">
+        <li>Home</li>
+        <li>About</li>
+        <li>Flights</li>
+        <li>Destinations</li>
+        <li>Blogs</li>
+        <li>Contact</li>
+      </ul>
     </div>
   );
 };
