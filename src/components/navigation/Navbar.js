e5764d7f83342cbb086e138392cfe4e1cebb1ff5
德华 刘 <gavinprior5@gmail.com>
2024-04-18 16:58:15 +0000

    set state for mobile menu

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../Logo";
 import NavbarLinks from "./NavbarLinks";
 import SocialMediaLinks from "./SocialMediaLinks";
 
 const Navbar = () => {
+  // ----- handling state for hamburger menu span
+  const [mobileMenu, setMobileMenu] = useState(false);
   return (
     <nav className="w-screen max-w-[1240px] h-24 bg-transparent z-10 m-auto flex items-center justify-between absolute top-0 px-10">
       {/* ----- Logo */}
@@ -14,9 +16,13 @@ const Navbar = () => {
       <SocialMediaLinks />
       {/* ----- hamburger menu */}
       <div className="ml-auto w-6 aspect-square relative">
+        {/* ---- first & last span - opacity 0, width 0 when open */}
         <span className="bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-0/4"></span>
+        {/* ----- Rotate 45 deg when open */}
         <span className="bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-1/4"></span>
+        {/* ----- Rotate -45 deg when open */}
         <span className="bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-1/4"></span>
+        {/* ---- first & last span - opacity 0, width 0 when open */}
         <span className="bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-2/4"></span>
       </div>
     </nav>
