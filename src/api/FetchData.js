6a3e4c7dea67c4c1c0cec677f4efac8323f45782
德华 刘 <gavinprior5@gmail.com>
2024-04-18 20:48:14 +0000

    install axios

--- src/api/FetchData.js
+++ src/api/FetchData.js
@@ -1,5 +1,16 @@
 import { useEffect, useState } from "react";
 
 const FetchData = (endpoint) => {
-    
-}
+  //  Define state variables to manage fetched data, loading state, and errors
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errors, setErros] = useState(null);
+
+  useEffect(() => {
+    // Define an asynchronous function to fetch data from the specified endpoint
+    const fetchData = async () => {
+      try {
+      } catch (error) {}
+    };
+  });
+};
