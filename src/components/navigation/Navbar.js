b16a24c86d786230e7568020ffcc4477dd0a3fa2
德华 刘 <gavinprior5@gmail.com>
2024-04-18 17:13:05 +0000

    style hamburger spans to animate on click

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -25,13 +25,37 @@ const Navbar = () => {
         onClick={handleMobileMenu}
       >
         {/* ---- first & last span - opacity 0, width 0 when open */}
-        <span className="bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-0/4"></span>
+        <span
+          className={
+            mobileMenu
+              ? `opacity-0 w-0 bg-yellow h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-0/4`
+              : `bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-0/4`
+          }
+        ></span>
         {/* ----- Rotate 45 deg when open */}
-        <span className="bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-1/4"></span>
+        <span
+          className={
+            mobileMenu
+              ? `-rotate-45 bg-yellow w-full h-0.5 rounded-full absolute transition-all duration-500 ease-in-out top-1/4`
+              : `bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-1/4`
+          }
+        ></span>
         {/* ----- Rotate -45 deg when open */}
-        <span className="bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-1/4"></span>
+        <span
+          className={
+            mobileMenu
+              ? `rotate-45 bg-yellow w-full h-0.5 rounded-full absolute transition-all duration-500 ease-in-out top-1/4`
+              : `bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-1/4`
+          }
+        ></span>
         {/* ---- first & last span - opacity 0, width 0 when open */}
-        <span className="bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-2/4"></span>
+        <span
+          className={
+            mobileMenu
+              ? `opacity-0 w-0 bg-yellow h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-2/4`
+              : `bg-yellow w-full h-0.5 rounded-full absolute rotate-0 transition-all duration-500 ease-in-out top-2/4`
+          }
+        ></span>
       </div>
     </nav>
   );
