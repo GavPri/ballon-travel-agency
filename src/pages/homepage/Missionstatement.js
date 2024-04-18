e2f596fa9d65a3d698d8bd161c7ffe0acb97b60b
德华 刘 <gavinprior5@gmail.com>
2024-04-18 21:21:36 +0000

    create missionstatement.js

---
+++ src/pages/homepage/Missionstatement.js
@@ -0,0 +1,9 @@
+import React from "react";
+import FetchData from "../../api/FetchData";
+import ProcessData from "../../api/ProcessData";
+
+const Missionstatement = () => {
+  return <div>Missionstatement</div>;
+};
+
+export default Missionstatement;
