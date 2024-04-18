b79e9613a58b9f5d9247284eb7ba1b2fe149808e
德华 刘 <gavinprior5@gmail.com>
2024-04-18 11:48:17 +0000

    update tailwind config to hold fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        M: ["Merienda", "sans-serif"],
+        Fig: ["Figtree", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
