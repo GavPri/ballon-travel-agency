e894fb55bbf9e11e741a3f9a8842eb48a6923923
德华 刘 <gavinprior5@gmail.com>
2024-04-18 12:33:48 +0000

    add ul and basic styling to nav

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -3,10 +3,18 @@ import Logo from "../Logo";
 
 const Navbar = () => {
   return (
-    <nav className="max-w-[1240px] bg-dark h-24 m-auto">
+    <nav className="max-w-[1240px] bg-dark h-24 m-auto flex">
       {/* ----- Logo */}
       <Logo width={100} height={100} />
       {/* ----- Nav links / unordered list */}
+      <ul className="text-grey flex items-center">
+        <li>Home</li>
+        <li>About</li>
+        <li>Flights</li>
+        <li>Destination</li>
+        <li>Blogs</li>
+        <li>Contact</li>
+      </ul>
       {/* ----- Social media links */}
     </nav>
   );
