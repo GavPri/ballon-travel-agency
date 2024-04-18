784b7f27ddbe0d89aaa262e0c9ddb51094ad95b3
德华 刘 <gavinprior5@gmail.com>
2024-04-18 13:28:58 +0000

    import hero image to Hero.js

--- src/pages/homepage/Hero.js
+++ src/pages/homepage/Hero.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import hero from "../../images/hero.jpg";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  return <div>Hero</div>;
+};
 
-export default Hero
+export default Hero;
