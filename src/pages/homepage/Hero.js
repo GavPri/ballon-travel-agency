9379139aa2299c8ac9738f2cb94f943e45231274
德华 刘 <gavinprior5@gmail.com>
2024-04-18 15:06:58 +0000

    add banner text to hero image
    add style & utility class to text

--- src/pages/homepage/Hero.js
+++ src/pages/homepage/Hero.js
@@ -4,12 +4,22 @@ import BackgroundImage from "../../components/BackgroundImage";
 
 const Hero = () => {
   return (
-    <div className="max-w-[1240px] m-auto h-[800px] relative mt-4">
+    <div className="max-w-[1240px] m-auto h-[800px] relative mt-4 flex justify-center items-center">
       <BackgroundImage
         imageUrl={hero}
         height={800}
         className={`rounded-lg absolute`}
       />
+      <div className="z-10 ">
+        <h2 className="font-M text-yellow text-5xl uppercase text-center">
+          Emb<span className="stand-out">a</span>rk on a journe
+          <span className="stand-out">
+            y<br></br>
+          </span>{" "}
+          of a lif<span className="stand-out">e</span>
+          time
+        </h2>
+      </div>
     </div>
   );
 };
