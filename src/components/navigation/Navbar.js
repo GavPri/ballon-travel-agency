31c799d38d6b92d476b7d215d64008016404c67d
德华 刘 <gavinprior5@gmail.com>
2024-04-18 13:11:28 +0000

    create utility class for list items
    add it to list items in nav

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -13,36 +13,24 @@ const Navbar = () => {
       <Logo width={100} height={100} />
       {/* ----- Nav links / unordered list */}
       <ul className="text-grey flex  font-Fig">
-        <li className="mr-6 hover:cursor-pointer hover:text-yellow transition-all duration-500 ease-in-out">
-          Home
-        </li>
-        <li className="mr-6 hover:cursor-pointer hover:text-yellow transition-all duration-500 ease-in-out">
-          About
-        </li>
-        <li className="mr-6 hover:cursor-pointer hover:text-yellow transition-all duration-500 ease-in-out">
-          Flights
-        </li>
-        <li className="mr-6 hover:cursor-pointer hover:text-yellow transition-all duration-500 ease-in-out">
-          Destination
-        </li>
-        <li className="mr-6 hover:cursor-pointer hover:text-yellow transition-all duration-500 ease-in-out">
-          Blogs
-        </li>
-        <li className="hover:cursor-pointer hover:text-yellow transition-all duration-500 ease-in-out">
-          Contact
-        </li>
+        <li className="mr-6 list-items-hover">Home</li>
+        <li className="mr-6 list-items-hover">About</li>
+        <li className="mr-6 list-items-hover">Flights</li>
+        <li className="mr-6 list-items-hover">Destination</li>
+        <li className="mr-6 list-items-hover">Blogs</li>
+        <li className="list-items-hover">Contact</li>
       </ul>
       {/* ----- Social media links */}
       <div className="flex text-white items-center">
         <p className="font-Fig text-sm">Follow Us :</p>
         <ul className="flex text">
-          <li className="mx-2 hover:cursor-pointer hover:text-yellow transition-all duration-500 ease-in-out">
+          <li className="mx-2 list-items-hover">
             <AiFillFacebook size={20} />
           </li>
-          <li className="mx-2 hover:cursor-pointer hover:text-yellow transition-all duration-500 ease-in-out">
+          <li className="mx-2 list-items-hover">
             <AiFillTwitterSquare size={20} />
           </li>
-          <li className="mx-2 hover:cursor-pointer hover:text-yellow transition-all duration-500 ease-in-out">
+          <li className="mx-2 list-items-hover">
             <AiFillInstagram size={20} />
           </li>
         </ul>
