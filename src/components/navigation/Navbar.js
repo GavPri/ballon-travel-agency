b99a027968de49cb8f040c60cd3e4e5f15e6b5c1
德华 刘 <gavinprior5@gmail.com>
2024-04-18 20:17:31 +0000

    remove unused import from Navbar.js

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Logo from "../Logo";
 import NavbarLinks from "./NavbarLinks";
 import SocialMediaLinks from "./SocialMediaLinks";
