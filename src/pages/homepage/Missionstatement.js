b20c85855dd4d44950faaa0770a3d16758953d25
德华 刘 <gavinprior5@gmail.com>
2024-04-20 09:52:19 +0000

    initial styling of mission statement header

--- src/pages/homepage/Missionstatement.js
+++ src/pages/homepage/Missionstatement.js
@@ -3,7 +3,19 @@ import FetchData from "../../api/FetchData";
 import ProcessData from "../../api/ProcessData";
 
 const Missionstatement = () => {
-  return <div>Missionstatement</div>;
+  return (
+    // ----- Wrapping Div
+    <div className="max-w-[1240px] m-auto">
+      {/* ----- Section Header */}
+      <div>
+        <h2 className="uppercase text-xl">
+          Th<span className="stand-out">e</span> Art of <br></br> tail
+          <span className="stand-out">o</span>red tr
+          <span className="stand-out">a</span>vel
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Missionstatement;
