233c171af51aada2ad6f9846a33ce53a8ed8f5ce
德华 刘 <gavinprior5@gmail.com>
2024-04-20 12:28:36 +0000

    render icon in map function

--- src/pages/homepage/Missionstatement.js
+++ src/pages/homepage/Missionstatement.js
@@ -19,6 +19,14 @@ const Missionstatement = () => {
   if (errors) {
     return <div>Oops, there was an error... {errors.message}</div>;
   }
+
+  // Array of Icons to Loop through
+  const MissionIcons = [
+    <PiTreeEvergreenBold size={30} />,
+    <IoLocationOutline size={30} />,
+    <TbRobot size={30} />,
+  ];
+
   return (
     // ----- Wrapping Div
     <div className="max-w-[1240px] m-auto">
@@ -33,24 +41,30 @@ const Missionstatement = () => {
       {/* ----- Mapping Over fetched data */}
       <div className="w-3/4 m-auto">
         {data &&
-          Object.values(data.company_info.missions_and_stats).map((mission) => (
-            <div
-              key={mission.heading}
-              className="text-white mb-8 max-w-readable"
-            >
-              {/* ------ H3 container */}
-              <div className="flex justify-center items-between">
-                <div className="h-16 aspect-square bg-transparent relative mr-6 flex justify-center items-center">
-                  <div className="h-10 border-2 border-[#013220] aspect-square rounded-lg top-0 left-0 absolute"></div>
-                  <div className="h-10 aspect-square rounded-lg bg-[#013220] right-0  bottom-1 absolute"></div>
+          Object.values(data.company_info.missions_and_stats).map(
+            (mission, index) => (
+              <div
+                key={mission.heading}
+                className="text-white mb-8 max-w-readable"
+              >
+                {/* ------ H3 container */}
+                <div className="flex justify-center items-between">
+                  <div className="h-16 aspect-square bg-transparent relative mr-6 flex justify-center items-center">
+                    <div className=" text-yellow h-16 flex justify-center items-center z-10">
+                      {MissionIcons[index % MissionIcons.length]}
+                    </div>
+
+                    <div className="h-10 border-2 border-[#013220] aspect-square rounded-lg top-0 left-0 absolute"></div>
+                    <div className="h-10 aspect-square rounded-lg bg-[#013220] right-0  bottom-1 absolute"></div>
+                  </div>
+                  <h3 className="mb-4 text-2xl font-Fig header-spacing">
+                    {mission.heading}
+                  </h3>
                 </div>
-                <h3 className="mb-4 text-2xl font-Fig header-spacing">
-                  {mission.heading}
-                </h3>
+                <p className="font-Fig para-spacing">{mission.description}</p>
               </div>
-              <p className="font-Fig para-spacing">{mission.description}</p>
-            </div>
-          ))}
+            )
+          )}
       </div>
     </div>
   );
