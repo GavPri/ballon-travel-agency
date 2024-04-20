180975c758d5f0e1e1e78dd626a980a14332d2f1
德华 刘 <gavinprior5@gmail.com>
2024-04-20 09:55:59 +0000

    import mission statement to app.js
    additional styling to section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/navigation/Navbar";
 import Hero from "../src/pages/homepage/Hero";
+import Missionstatement from "./pages/homepage/Missionstatement";
 
 function App() {
   return (
     <div className="max-w-[1240px] m-auto">
       <Navbar />
       <Hero />
+      <Missionstatement />
     </div>
   );
 }
--- src/pages/homepage/Missionstatement.js
+++ src/pages/homepage/Missionstatement.js
@@ -7,8 +7,8 @@ const Missionstatement = () => {
     // ----- Wrapping Div
     <div className="max-w-[1240px] m-auto">
       {/* ----- Section Header */}
-      <div>
-        <h2 className="uppercase text-xl">
+      <div className="w-full text-center">
+        <h2 className="uppercase text-5xl text-yellow font-M">
           Th<span className="stand-out">e</span> Art of <br></br> tail
           <span className="stand-out">o</span>red tr
           <span className="stand-out">a</span>vel
