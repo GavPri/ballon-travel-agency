af122c82773e8bbb9ac51c636e70dfcb0eb3c4a7
德华 刘 <gavinprior5@gmail.com>
2024-04-22 12:43:21 +0000

    map over continents data
    fix error state - add return

--- src/pages/homepage/ContinentsCarousel.js
+++ src/pages/homepage/ContinentsCarousel.js
@@ -1,27 +1,38 @@
 import React from "react";
-// ----- import fetch data hook. 
+// ----- import fetch data hook.
 import FetchData from "../../api/FetchData";
 
 const ContinentsCarousel = () => {
-    // ----- fetch and deconstruct data from api end point.
-    const { data, isLoading, errors} = FetchData('gavpri/ballon-travel-agency/db')
+  // ----- fetch and deconstruct data from api end point.
+  const { data, isLoading, errors } = FetchData(
+    "gavpri/ballon-travel-agency/db"
+  );
 
-    // ----- set loading states 
-    if (isLoading) {
-        return <div>...Loading</div>
-    }
-    // ----- set errors state 
-    if (errors) {
-        <div>Oops, there was an error... {errors.message}</div>
-    }
-    
+  // ----- set loading states
+  if (isLoading) {
+    return <div>...Loading</div>;
+  }
+  // ----- set errors state
+  if (errors) {
+    return <div>Oops, there was an error... {errors.message}</div>;
+  }
+
   return (
     <div className="max-w-[90vw] m-auto">
+      {/* ------ Heading  */}
       <h2 className="uppercase text-yellow font-M header-spacing text-xl text-center">
         Disc<span className="stand-out">o</span>ver popula
         <span className="stand-out">r</span> Destin
         <span className="stand-out">a</span>tions
       </h2>
+      {/* ----- Continent Tabs */}
+      <div>
+        {data.continents.map((continent, index) => (
+          <div key={index}>
+            {continent.name} - {continent.slug}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
