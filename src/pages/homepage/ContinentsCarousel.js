91ddbbd24851e7fb0ba47b2bf869e09673084318
德华 刘 <gavinprior5@gmail.com>
2024-04-22 15:08:04 +0000

    add style to continent cards

--- src/pages/homepage/ContinentsCarousel.js
+++ src/pages/homepage/ContinentsCarousel.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 // ----- import fetch data hook.
 import FetchData from "../../api/FetchData";
+import { FaLocationPin } from "react-icons/fa6";
 
 const ContinentsCarousel = () => {
   // ----- fetch and deconstruct data from api end point.
@@ -39,7 +40,7 @@ const ContinentsCarousel = () => {
         <span className="stand-out">a</span>tions
       </h2>
       {/* ----- Continent Tabs */}
-      <div className="flex flex-wrap gap-1 justify-center">
+      <div className="flex flex-wrap gap-1 justify-center mb-4">
         {data.continents.map((continent, index) => (
           <div
             onClick={() => handleIndex(index)}
@@ -65,8 +66,20 @@ const ContinentsCarousel = () => {
                   <img
                     src={location.image}
                     alt={location.name}
-                    className="w-full h-full object-cover absolute top-0 left-0"
+                    className="w-full h-full object-cover absolute top-0 left-0 brightness-50"
                   />
+                  <div className="w-full bg-gradient-to-b from-transparent to-dark mb-auto h-1/2 absolute bottom-0">
+                    <h3 className="text-center font-Fig text-yellow text-bold mt-2">
+                      {location.name}
+                    </h3>
+                    <div className="w-full flex justify-between items-center px-2">
+                      <p className="text-yellow font-Fig flex items-center">
+                        <FaLocationPin size={20} />
+                        <span className="text-white">{location.country}</span>
+                      </p>
+                      <p className="font-Fig text-white">{location.price}</p>
+                    </div>
+                  </div>
                 </div>
               ))}
             </div>
