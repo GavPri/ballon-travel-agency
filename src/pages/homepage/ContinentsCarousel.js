7ead349d7c8e3131131d34ce3ed84d3e67d5921c
德华 刘 <gavinprior5@gmail.com>
2024-04-22 12:16:47 +0000

    style continents carousel heading

--- src/pages/homepage/ContinentsCarousel.js
+++ src/pages/homepage/ContinentsCarousel.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const ContinentsCarousel = () => {
   return (
-    <div>ContinentsCarousel</div>
-  )
-}
+    <div className="max-w-[90vw] m-auto">
+      <h2 className="uppercase text-yellow font-M header-spacing text-xl text-center">
+        Disc<span className="stand-out">o</span>ver popula
+        <span className="stand-out">r</span> Destin
+        <span className="stand-out">a</span>tions
+      </h2>
+    </div>
+  );
+};
 
-export default ContinentsCarousel
+export default ContinentsCarousel;
