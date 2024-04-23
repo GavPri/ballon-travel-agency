aa3ee391655fe881e3e9798e910003656bb9f2e9
德华 刘 <gavinprior5@gmail.com>
2024-04-23 15:31:09 +0000

    remove indivdual countries info from db
    max lenght exceeded

---
+++ src/pages/homepage/CountryCarousel.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import FetchData from '../../api/FetchData'
+
+const CountryCarousel = () => {
+  return (
+    <div>CountryCarousel</div>
+  )
+}
+
+export default CountryCarousel
